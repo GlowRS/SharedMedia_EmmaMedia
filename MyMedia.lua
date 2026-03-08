4e8907ef76358d3403567433d36fae7068e3ca05
Emma <konfoninja5@gmail.com>
2026-03-07 19:54:19 -0700

    v1

---
+++ MyMedia.lua
@@ -0,0 +1,22 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "Twist", [[Interface\Addons\SharedMedia_EmmaMedia\sound\Twist.mp3]]) 
+
+-- -----
+--   STATUSBAR
+-- -----
